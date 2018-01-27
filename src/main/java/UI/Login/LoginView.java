c9de12079d1e562d8c38fdb6e299f4809f7b02d7
GonzaV <vieygonzalo@gmail.com>
2018-01-27 00:52:00 -0300

    Arreglado el login?

--- src/main/java/UI/Login/LoginView.java
+++ src/main/java/UI/Login/LoginView.java
@@ -1,17 +1,6 @@
 package UI.Login;
 
 import de.saxsys.mvvmfx.FxmlView;
-import javafx.fxml.Initializable;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class LoginView implements FxmlView<LoginViewModel>, Initializable {
-
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-    }
-
+public class LoginView implements FxmlView<LoginViewModel> {
 }
