8e50a7313e28b9f91057bda440d2469ee52f13d0
GonzaV <vieygonzalo@gmail.com>
2018-01-29 12:55:33 -0300

    Probando configuracion para distintas resoluciones

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application
 {
     public static void main(String...args){
@@ -19,14 +21,42 @@ public class Main extends Application
 
         //Nombre del escenario
         primaryStage.setTitle("Test MMPI 2");
+
         //primaryStage.setResizable(false);
+
+        Dimension screenSeize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSeize.getWidth();
+        double height = screenSeize.getHeight();
+
+        if(width == 1280.0 && height == 1024.0) {
+
         primaryStage.setMaxHeight(700);
         primaryStage.setMaxWidth(1125);
         primaryStage.setMinHeight(450);
         primaryStage.setMinWidth(600);
         primaryStage.setHeight(700);
-        
         primaryStage.setWidth(1125);
+
+        }
+
+        else {
+
+            if (width == 1920.0 && height == 1080.0){
+
+                primaryStage.setMaxHeight(830);
+                primaryStage.setMaxWidth(1545);
+                primaryStage.setMinHeight(450);
+                primaryStage.setMinWidth(600);
+                primaryStage.setHeight(830);
+                primaryStage.setWidth(1545);
+
+            }
+
+        }
+
+
+        System.out.println(width);
+        System.out.println(height);
 
         //Obtengo el FXML
         ViewTuple<LoginView, LoginViewModel> viewTuple = FluentViewLoader.fxmlView(LoginView.class).load();
