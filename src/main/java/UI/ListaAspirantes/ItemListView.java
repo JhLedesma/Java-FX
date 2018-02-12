c7e7ee345e934ee89357eb4b048a01e1cb9f7ce2
Jesus Ledesma <jh.ledesmaa@gmail.com>
2018-02-12 15:01:38 -0300

    El popup se abre y cierra al hacer clic, antes se creaban mas items

--- src/main/java/UI/ListaAspirantes/ItemListView.java
+++ src/main/java/UI/ListaAspirantes/ItemListView.java
@@ -4,25 +4,14 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXNodesList;
 import com.jfoenix.controls.JFXPopup;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 
-import javafx.event.ActionEvent;
-import javafx.scene.text.Font;
 
 public abstract class ItemListView implements FxmlView<ItemListViewModel>
 {
@@ -57,13 +46,14 @@ public abstract class ItemListView implements FxmlView<ItemListViewModel>
     private Label titulacion;
 
     @FXML
-    private JFXNodesList node;
-
-    @FXML
     private JFXButton opcion;
 
     @FXML
     private JFXListView<Label> listView;
+
+    JFXPopup popup = new JFXPopup();
+
+    boolean popupAbierto = false;
 
 
     public void initialize()
@@ -77,56 +67,48 @@ public abstract class ItemListView implements FxmlView<ItemListViewModel>
         especialista.textProperty().bind(viewModel.especialistaProperty());
         diagnostico.textProperty().bind(viewModel.diagnosticoProperty());
         titulacion.textProperty().bind(viewModel.titulacionProperty());
-
-        JFXButton a = new JFXButton("Opciones");
-        a.setGraphic(new FontAwesomeIconView(FontAwesomeIcon.PLUS));
-        a.setButtonType(JFXButton.ButtonType.RAISED);
-        JFXButton b = new JFXButton("R2");
-        b.setGraphic(new MaterialDesignIconView(MaterialDesignIcon.ACCOUNT_CIRCLE));
-        b.setButtonType(JFXButton.ButtonType.RAISED);
-        JFXButton c = new JFXButton("R3");
-        c.setGraphic(new MaterialDesignIconView(MaterialDesignIcon.ACCOUNT_CIRCLE));
-        c.setButtonType(JFXButton.ButtonType.RAISED);
-
-
-        node.addAnimatedNode(a);
-        node.addAnimatedNode(b);
-        node.addAnimatedNode(c);
-        node.setRotate(90);
-        node.setSpacing(50);
     }
 
+//----------------------Popup------------------------//
     @FXML
     public void exec(MouseEvent mouseEvent) {
-        JFXButton b = new JFXButton("hola");
-        JFXButton c = new JFXButton("h");
-        JFXButton a = new JFXButton("hol");
-        VBox vBox = new VBox(a, b, c);
 
-        this.init(listView);
-        listView.setVerticalGap(10.0);
-        listView.depthProperty().set(1);
-        listView.setExpanded(true);
+        if(!popupAbierto){
 
-        JFXPopup p = new JFXPopup(listView);
-        p.show(opcion, JFXPopup.PopupVPosition.TOP, JFXPopup.PopupHPosition.LEFT, -190.0,10.0 );
-        listView.setVisible(true);
+            this.loadDateListView();
+            this.showPopup();
+            popupAbierto = true;
 
+        }else{
 
-
+            popup.hide();
+            popupAbierto = false;
+            listView.getItems().clear();
+        }
 
     }
 
-    private void init(JFXListView list)
+
+    private void loadDateListView()
     {
-
-
         for(int i=0; i<4;i++)
         {
             Label lbl = new Label("item" +i);
             lbl.setGraphic(new MaterialDesignIconView(MaterialDesignIcon.ACCOUNT_CIRCLE));
-            list.getItems().add(lbl);
+            listView.getItems().add(lbl);
         }
+    }
+
+
+    private void showPopup() {
+        listView.setVerticalGap(10.0);
+        listView.depthProperty().set(1);
+        listView.setExpanded(true);
+
+        popup.setPopupContent(listView);
+        popup.setStyle("-fx-background-color: transparent;");
+        popup.show(opcion, JFXPopup.PopupVPosition.TOP, JFXPopup.PopupHPosition.LEFT, -190.0,10.0 );
+        listView.setVisible(true);
     }
 
 
--- src/main/java/UI/Main.java
+++ src/main/java/UI/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application
 
         GestorScenas.setStageApp(primaryStage);
 
-        GestorScenas.getFamily().showInicio();
+        GestorScenas.getFamily().showListaAspirantes();
 
     }
 
