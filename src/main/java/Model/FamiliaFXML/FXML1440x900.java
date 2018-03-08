fc272ab548116b1f8d066cdbcaff7d7271cf07fd
kechum <jh.ledesmaa@gmail.com>
2018-03-08 00:04:43 -0300

    Se declaran todas Resoluciones que el programa usara

---
+++ src/main/java/Model/FamiliaFXML/FXML1440x900.java
@@ -0,0 +1,48 @@
+package Model.FamiliaFXML;
+
+public class FXML1440x900 extends FamilyFXML
+{
+    public FXML1440x900(int maxHeight, int maxWidth, int minHeight, int minWidth, int height, int width) {
+        super(maxHeight, maxWidth, minHeight, minWidth, height, width);
+    }
+
+    @Override
+    public void showLogin() {
+
+    }
+
+    @Override
+    public void showListaAspirantes() {
+
+    }
+
+    @Override
+    public void showInicio() {
+
+    }
+
+    @Override
+    public void showMenuEspecialista() {
+
+    }
+
+    @Override
+    public void showInputNormal() {
+
+    }
+
+    @Override
+    public void showConfiguraciones() {
+
+    }
+
+    @Override
+    public void showIngresoDatosAspirante() {
+
+    }
+
+    @Override
+    public void showInputAlternativo() {
+
+    }
+}
--- src/main/java/Model/GestorScenas.java
+++ src/main/java/Model/GestorScenas.java
@@ -50,22 +50,25 @@ public class GestorScenas
         double width = screenSeize.getWidth();
         double height = screenSeize.getHeight();
 
-        if (width >= 1920.0 && height >= 1080.0)
+        if (width > 1440.0 && height > 900.0)
         {
-            familyFXML = new FXML1920x1080(850, 1545, 450, 600, 840, 1545);
+            familyFXML = new FXML1920x1080(840, 1545, 840, 1545, 840, 1545);
+        }
+        else if (width > 1360.0 && height > 765.0)
+        {
+            //familyFXML = new FXML1440x900() Decidir Numeros de Pantalla
+        }
+        else if (width > 1280.0 && height > 1024.0)
+        {
+            familyFXML = new FXML1360x765(600,1250,600,1250,600,1250);
+        }
+        else if (width > 1024.0 && height > 798.0)
+        {
+            familyFXML = new FXML1280x1024(750, 1125, 750, 1125, 750, 1125);
         }
         else
         {
-            if (width == 1280.0 && height == 1024.0)
-            {
-                familyFXML = new FXML1280x1024(750, 1125, 450, 600, 750, 1125);
-            }
-
-            else
-            {
-                familyFXML = new FXML1360x765(600,1250,450,600,600,1250);
-            }
-
+            //familyFXML = FXML1024x798() Decidir Numeros de Pantalla
         }
     }
 
