e477b4e5d2db6125905a5b4ffd6ec9133e6ae1a9
kechum <jh.ledesmaa@gmail.com>
2018-04-12 01:53:45 -0300

    ItemEscala Histeria

---
+++ src/main/java/Model/Escalas/Histeria.java
@@ -0,0 +1,75 @@
+package Model.Escalas;
+
+public class Histeria extends Escala
+{
+    @Override
+    protected void iniciarItemsEscala()
+    {
+        //60 Items: 13V y 47F
+        itemsEscala.add(new ItemEscala(11,1));
+        itemsEscala.add(new ItemEscala(18, 1));
+        itemsEscala.add(new ItemEscala(31, 1));
+        itemsEscala.add(new ItemEscala(39, 1));
+        itemsEscala.add(new ItemEscala(40, 1));
+        itemsEscala.add(new ItemEscala(44, 1));
+        itemsEscala.add(new ItemEscala(65, 1));
+        itemsEscala.add(new ItemEscala(101, 1));
+        itemsEscala.add(new ItemEscala(166, 1));
+        itemsEscala.add(new ItemEscala(172, 1));
+        itemsEscala.add(new ItemEscala(175, 1));
+        itemsEscala.add(new ItemEscala(218, 1));
+        itemsEscala.add(new ItemEscala(230, 1));
+        itemsEscala.add(new ItemEscala(2, 0));
+        itemsEscala.add(new ItemEscala(3, 0));
+        itemsEscala.add(new ItemEscala(7, 0));
+        itemsEscala.add(new ItemEscala(8, 0));
+        itemsEscala.add(new ItemEscala(9, 0));
+        itemsEscala.add(new ItemEscala(10,0));
+        itemsEscala.add(new ItemEscala(14, 0));
+        itemsEscala.add(new ItemEscala(26, 0));
+        itemsEscala.add(new ItemEscala(29, 0));
+        itemsEscala.add(new ItemEscala(45, 0));
+        itemsEscala.add(new ItemEscala(47, 0));
+        itemsEscala.add(new ItemEscala(58, 0));
+        itemsEscala.add(new ItemEscala(76, 0));
+        itemsEscala.add(new ItemEscala(81,0));
+        itemsEscala.add(new ItemEscala(91, 0));
+        itemsEscala.add(new ItemEscala(95, 0));
+        itemsEscala.add(new ItemEscala(98, 0));
+        itemsEscala.add(new ItemEscala(110, 0));
+        itemsEscala.add(new ItemEscala(115, 0));
+        itemsEscala.add(new ItemEscala(116, 0));
+        itemsEscala.add(new ItemEscala(124, 0));
+        itemsEscala.add(new ItemEscala(125, 0));
+        itemsEscala.add(new ItemEscala(129,0));
+        itemsEscala.add(new ItemEscala(135, 0));
+        itemsEscala.add(new ItemEscala(141, 0));
+        itemsEscala.add(new ItemEscala(148, 0));
+        itemsEscala.add(new ItemEscala(151, 0));
+        itemsEscala.add(new ItemEscala(152, 0));
+        itemsEscala.add(new ItemEscala(157, 0));
+        itemsEscala.add(new ItemEscala(159, 0));
+        itemsEscala.add(new ItemEscala(161, 0));
+        itemsEscala.add(new ItemEscala(164, 0));
+        itemsEscala.add(new ItemEscala(167, 0));
+        itemsEscala.add(new ItemEscala(173, 0));
+        itemsEscala.add(new ItemEscala(176, 0));
+        itemsEscala.add(new ItemEscala(179, 0));
+        itemsEscala.add(new ItemEscala(185, 0));
+        itemsEscala.add(new ItemEscala(193, 0));
+        itemsEscala.add(new ItemEscala(208, 0));
+        itemsEscala.add(new ItemEscala(213, 0));
+        itemsEscala.add(new ItemEscala(224, 0));
+        itemsEscala.add(new ItemEscala(241, 0));
+        itemsEscala.add(new ItemEscala(243, 0));
+        itemsEscala.add(new ItemEscala(249, 0));
+        itemsEscala.add(new ItemEscala(253, 0));
+        itemsEscala.add(new ItemEscala(263, 0));
+        itemsEscala.add(new ItemEscala(265, 0));
+    }
+
+    @Override
+    protected void inicarPT() {
+
+    }
+}
