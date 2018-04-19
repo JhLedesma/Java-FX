6012e08a9acbcbe538c379e4b3143268546b44de
kechum <jh.ledesmaa@gmail.com>
2018-04-19 11:42:00 -0300

    Escala Esquizofrenia

--- src/main/java/Model/Escalas/MF_Mujer.java
+++ src/main/java/Model/Escalas/MF_Mujer.java
@@ -3,8 +3,64 @@ package Model.Escalas;
 public class MF_Mujer extends Escala
 {
     @Override
-    protected void iniciarItemsEscala() {
-
+    protected void iniciarItemsEscala()
+    {
+        //56 Items: 25V y 31F
+        itemsEscala.add(new ItemEscala(4,1));
+        itemsEscala.add(new ItemEscala(25,1));
+        itemsEscala.add(new ItemEscala(62,1));
+        itemsEscala.add(new ItemEscala(64,1));
+        itemsEscala.add(new ItemEscala(67,1));
+        itemsEscala.add(new ItemEscala(74,1));
+        itemsEscala.add(new ItemEscala(80,1));
+        itemsEscala.add(new ItemEscala(112,1));
+        itemsEscala.add(new ItemEscala(119,1));
+        itemsEscala.add(new ItemEscala(121,1));
+        itemsEscala.add(new ItemEscala(128,1));
+        itemsEscala.add(new ItemEscala(137,1));
+        itemsEscala.add(new ItemEscala(177,1));
+        itemsEscala.add(new ItemEscala(187,1));
+        itemsEscala.add(new ItemEscala(191,1));
+        itemsEscala.add(new ItemEscala(196,1));
+        itemsEscala.add(new ItemEscala(205,1));
+        itemsEscala.add(new ItemEscala(219,1));
+        itemsEscala.add(new ItemEscala(236,1));
+        itemsEscala.add(new ItemEscala(251,1));
+        itemsEscala.add(new ItemEscala(256,1));
+        itemsEscala.add(new ItemEscala(271,1));
+        itemsEscala.add(new ItemEscala(1,0));
+        itemsEscala.add(new ItemEscala(19,0));
+        itemsEscala.add(new ItemEscala(26,0));
+        itemsEscala.add(new ItemEscala(27,0));
+        itemsEscala.add(new ItemEscala(63,0));
+        itemsEscala.add(new ItemEscala(68,0));
+        itemsEscala.add(new ItemEscala(69,0));
+        itemsEscala.add(new ItemEscala(76,0));
+        itemsEscala.add(new ItemEscala(86,0));
+        itemsEscala.add(new ItemEscala(103,0));
+        itemsEscala.add(new ItemEscala(104,0));
+        itemsEscala.add(new ItemEscala(107,0));
+        itemsEscala.add(new ItemEscala(120,0));
+        itemsEscala.add(new ItemEscala(132,0));
+        itemsEscala.add(new ItemEscala(133,0));
+        itemsEscala.add(new ItemEscala(163,0));
+        itemsEscala.add(new ItemEscala(166,0));
+        itemsEscala.add(new ItemEscala(184,0));
+        itemsEscala.add(new ItemEscala(193,0));
+        itemsEscala.add(new ItemEscala(194,0));
+        itemsEscala.add(new ItemEscala(197,0));
+        itemsEscala.add(new ItemEscala(199,0));
+        itemsEscala.add(new ItemEscala(201,0));
+        itemsEscala.add(new ItemEscala(207,0));
+        itemsEscala.add(new ItemEscala(209,0));
+        itemsEscala.add(new ItemEscala(231,0));
+        itemsEscala.add(new ItemEscala(235,0));
+        itemsEscala.add(new ItemEscala(237,0));
+        itemsEscala.add(new ItemEscala(239,0));
+        itemsEscala.add(new ItemEscala(254,0));
+        itemsEscala.add(new ItemEscala(257,0));
+        itemsEscala.add(new ItemEscala(268,0));
+        itemsEscala.add(new ItemEscala(272,0));
     }
 
     @Override
