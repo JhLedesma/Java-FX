a4902e22380818a5584c0688d063fca3bbef36ec
GonzaV <vieygonzalo@gmail.com>
2018-04-26 10:04:50 -0300

    Puntaje T obsesividad

---
+++ src/main/java/Model/Escalas/Obsesividad.java
@@ -0,0 +1,50 @@
+package Model.Escalas;
+
+public class Obsesividad extends Escala {
+
+
+    @Override
+    protected void iniciarItemsEscala() {
+
+    }
+
+    @Override
+    protected void inicarPT() {
+
+        puntajeTMujer.put(0,30);
+        puntajeTMujer.put(1,33);
+        puntajeTMujer.put(2,36);
+        puntajeTMujer.put(3,39);
+        puntajeTMujer.put(4,42);
+        puntajeTMujer.put(5,45);
+        puntajeTMujer.put(6,48);
+        puntajeTMujer.put(7,52);
+        puntajeTMujer.put(8,54);
+        puntajeTMujer.put(9,57);
+        puntajeTMujer.put(10,60);
+        puntajeTMujer.put(11,63);
+        puntajeTMujer.put(12,66);
+        puntajeTMujer.put(13,69);
+        puntajeTMujer.put(14,72);
+        puntajeTMujer.put(15,75);
+        puntajeTMujer.put(16,78);
+
+        puntajeTVaron.put(0,33);
+        puntajeTVaron.put(1,36);
+        puntajeTVaron.put(2,39);
+        puntajeTVaron.put(3,42);
+        puntajeTVaron.put(4,44);
+        puntajeTVaron.put(5,47);
+        puntajeTVaron.put(6,50);
+        puntajeTVaron.put(7,53);
+        puntajeTVaron.put(8,56);
+        puntajeTVaron.put(9,58);
+        puntajeTVaron.put(10,61);
+        puntajeTVaron.put(11,64);
+        puntajeTVaron.put(12,67);
+        puntajeTVaron.put(13,69);
+        puntajeTVaron.put(14,72);
+        puntajeTVaron.put(15,75);
+        puntajeTVaron.put(16,78);
+    }
+}
