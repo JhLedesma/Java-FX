e331a04ac676cbeab00bec1453f30267f30750fe
GonzaV <vieygonzalo@gmail.com>
2018-08-07 13:01:18 -0300

    Se sigue perrito

---
+++ src/main/java/Model/Escalas/Contenido/Depresion_Escala_Contenido.java
@@ -0,0 +1,37 @@
+package Model.Escalas.Contenido;
+
+import Model.Escalas.Escala;
+
+public class Depresion_Escala_Contenido extends Escala {
+
+
+    @Override
+    protected void iniciarItemsEscala() {
+
+    }
+
+    @Override
+    protected void inicarPT() {
+
+        puntajeTVaron.put(0,35);
+        puntajeTVaron.put(1,36);
+        puntajeTVaron.put(2,38);
+        puntajeTVaron.put(3,40);
+        puntajeTVaron.put(4,42);
+        puntajeTVaron.put(5,44);
+        puntajeTVaron.put(6,46);
+        puntajeTVaron.put(7,48);
+        puntajeTVaron.put(8,50);
+        puntajeTVaron.put(9,52);
+        puntajeTVaron.put(10,53);
+        puntajeTVaron.put(11,55);
+        puntajeTVaron.put(12,57);
+        puntajeTVaron.put(13,59);
+        puntajeTVaron.put(14,61);
+        puntajeTVaron.put(15,63);
+        puntajeTVaron.put(16,65);
+        puntajeTVaron.put(17,67);
+
+
+    }
+}
