d2b4bd7d76d790a35a4e1113667da8f0875aec67
GonzaV <vieygonzalo@gmail.com>
2018-08-07 16:56:27 -0300

    Empiezo escala de cinismo, escalas de contenido

---
+++ src/main/java/Model/Escalas/Contenido/Cinismo.java
@@ -0,0 +1,31 @@
+package Model.Escalas.Contenido;
+
+import Model.Escalas.Escala;
+
+public class Cinismo extends Escala {
+    @Override
+    protected void iniciarItemsEscala() {
+
+    }
+
+    @Override
+    protected void inicarPT() {
+
+        puntajeTVaron.put(1,31);
+        puntajeTVaron.put(2,33);
+        puntajeTVaron.put(3,35);
+        puntajeTVaron.put(4,37);
+        puntajeTVaron.put(5,39);
+        puntajeTVaron.put(6,41);
+        puntajeTVaron.put(7,43);
+        puntajeTVaron.put(8,45);
+        puntajeTVaron.put(9,47);
+        puntajeTVaron.put(10,48);
+        puntajeTVaron.put(11,50);
+        puntajeTVaron.put(12,52);
+        puntajeTVaron.put(13,54);
+        puntajeTVaron.put(14,56);
+
+
+    }
+}
