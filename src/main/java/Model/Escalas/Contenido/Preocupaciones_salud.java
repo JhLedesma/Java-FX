78972490e909c95a6478fa947ce395cd982dc1e3
GonzaV <vieygonzalo@gmail.com>
2018-08-07 14:11:51 -0300

    Agrego escala preocupaciones_salud, escala de contenido

---
+++ src/main/java/Model/Escalas/Contenido/Preocupaciones_salud.java
@@ -0,0 +1,95 @@
+package Model.Escalas.Contenido;
+
+import Model.Escalas.Escala;
+
+public class Preocupaciones_salud extends Escala{
+
+
+    @Override
+    protected void iniciarItemsEscala() {
+
+    }
+
+    @Override
+    protected void inicarPT() {
+
+        puntajeTMujer.put(0,32);
+        puntajeTMujer.put(1,34);
+        puntajeTMujer.put(2,36);
+        puntajeTMujer.put(3,38);
+        puntajeTMujer.put(4,40);
+        puntajeTMujer.put(5,43);
+        puntajeTMujer.put(6,45);
+        puntajeTMujer.put(7,47);
+        puntajeTMujer.put(8,49);
+        puntajeTMujer.put(9,51);
+        puntajeTMujer.put(10,53);
+        puntajeTMujer.put(11,55);
+        puntajeTMujer.put(12,57);
+        puntajeTMujer.put(13,59);
+        puntajeTMujer.put(14,61);
+        puntajeTMujer.put(15,63);
+        puntajeTMujer.put(16,65);
+        puntajeTMujer.put(17,67);
+        puntajeTMujer.put(18,69);
+        puntajeTMujer.put(19,71);
+        puntajeTMujer.put(20,73);
+        puntajeTMujer.put(21,75);
+        puntajeTMujer.put(22,77);
+        puntajeTMujer.put(23,79);
+        puntajeTMujer.put(24,81);
+        puntajeTMujer.put(25,83);
+        puntajeTMujer.put(26,85);
+        puntajeTMujer.put(27,87);
+        puntajeTMujer.put(28,89);
+        puntajeTMujer.put(29,91);
+        puntajeTMujer.put(30,93);
+        puntajeTMujer.put(31,95);
+        puntajeTMujer.put(32,97);
+        puntajeTMujer.put(33,99);
+        puntajeTMujer.put(34,101);
+        puntajeTMujer.put(35,103);
+        puntajeTMujer.put(36,105);
+
+
+        puntajeTVaron.put(0,35);
+        puntajeTVaron.put(1,37);
+        puntajeTVaron.put(2,39);
+        puntajeTVaron.put(3,41);
+        puntajeTVaron.put(4,42);
+        puntajeTVaron.put(5,44);
+        puntajeTVaron.put(6,46);
+        puntajeTVaron.put(7,48);
+        puntajeTVaron.put(8,50);
+        puntajeTVaron.put(9,52);
+        puntajeTVaron.put(10,54);
+        puntajeTVaron.put(11,56);
+        puntajeTVaron.put(12,58);
+        puntajeTVaron.put(13,60);
+        puntajeTVaron.put(14,62);
+        puntajeTVaron.put(15,64);
+        puntajeTVaron.put(16,66);
+        puntajeTVaron.put(17,68);
+        puntajeTVaron.put(18,70);
+        puntajeTVaron.put(19,72);
+        puntajeTVaron.put(20,74);
+        puntajeTVaron.put(21,76);
+        puntajeTVaron.put(22,78);
+        puntajeTVaron.put(23,80);
+        puntajeTVaron.put(24,82);
+        puntajeTVaron.put(25,84);
+        puntajeTVaron.put(26,86);
+        puntajeTVaron.put(27,88);
+        puntajeTVaron.put(28,90);
+        puntajeTVaron.put(29,92);
+        puntajeTVaron.put(30,94);
+        puntajeTVaron.put(31,96);
+        puntajeTVaron.put(32,98);
+        puntajeTVaron.put(33,100);
+        puntajeTVaron.put(34,102);
+        puntajeTVaron.put(35,104);
+        puntajeTVaron.put(36,106);
+
+
+    }
+}
