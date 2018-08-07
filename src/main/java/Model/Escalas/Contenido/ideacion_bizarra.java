8c4cf2c0b7897366efbae99c8d9f2040be2bff1f
GonzaV <vieygonzalo@gmail.com>
2018-08-07 16:03:28 -0300

    Agrego escala ideacion_bizarra, escalas de contenido

---
+++ src/main/java/Model/Escalas/Contenido/ideacion_bizarra.java
@@ -0,0 +1,69 @@
+package Model.Escalas.Contenido;
+
+import Model.Escalas.Escala;
+
+public class ideacion_bizarra extends Escala {
+
+    @Override
+    protected void iniciarItemsEscala() {
+
+
+    }
+
+    @Override
+    protected void inicarPT() {
+
+        puntajeTVaron.put(0,39);
+        puntajeTVaron.put(1,42);
+        puntajeTVaron.put(2,45);
+        puntajeTVaron.put(3,48);
+        puntajeTVaron.put(4,51);
+        puntajeTVaron.put(5,54);
+        puntajeTVaron.put(6,57);
+        puntajeTVaron.put(7,60);
+        puntajeTVaron.put(8,63);
+        puntajeTVaron.put(9,66);
+        puntajeTVaron.put(10,69);
+        puntajeTVaron.put(11,72);
+        puntajeTVaron.put(12,75);
+        puntajeTVaron.put(13,78);
+        puntajeTVaron.put(14,81);
+        puntajeTVaron.put(15,84);
+        puntajeTVaron.put(16,87);
+        puntajeTVaron.put(17,90);
+        puntajeTVaron.put(18,92);
+        puntajeTVaron.put(19,95);
+        puntajeTVaron.put(20,98);
+        puntajeTVaron.put(21,101);
+        puntajeTVaron.put(22,104);
+        puntajeTVaron.put(23,107);
+
+
+        puntajeTMujer.put(0,39);
+        puntajeTMujer.put(1,42);
+        puntajeTMujer.put(2,46);
+        puntajeTMujer.put(3,49);
+        puntajeTMujer.put(4,52);
+        puntajeTMujer.put(5,56);
+        puntajeTMujer.put(6,59);
+        puntajeTMujer.put(7,62);
+        puntajeTMujer.put(8,66);
+        puntajeTMujer.put(9,69);
+        puntajeTMujer.put(10,72);
+        puntajeTMujer.put(11,75);
+        puntajeTMujer.put(12,79);
+        puntajeTMujer.put(13,82);
+        puntajeTMujer.put(14,85);
+        puntajeTMujer.put(15,89);
+        puntajeTMujer.put(16,92);
+        puntajeTMujer.put(17,95);
+        puntajeTMujer.put(18,99);
+        puntajeTMujer.put(19,102);
+        puntajeTMujer.put(20,105);
+        puntajeTMujer.put(21,109);
+        puntajeTMujer.put(22,112);
+        puntajeTMujer.put(23,115);
+        
+
+    }
+}
