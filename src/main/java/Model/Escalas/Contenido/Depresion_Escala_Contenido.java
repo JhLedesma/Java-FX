a5231f8541afa3694b9a59621d9ebe5dffe63a45
GonzaV <vieygonzalo@gmail.com>
2018-08-07 13:31:16 -0300

    Termino escala depresion, escala de contenido

--- src/main/java/Model/Escalas/Contenido/Depresion_Escala_Contenido.java
+++ src/main/java/Model/Escalas/Contenido/Depresion_Escala_Contenido.java
@@ -31,6 +31,58 @@ public class Depresion_Escala_Contenido extends Escala {
         puntajeTVaron.put(15,63);
         puntajeTVaron.put(16,65);
         puntajeTVaron.put(17,67);
+        puntajeTVaron.put(18,68);
+        puntajeTVaron.put(19,70);
+        puntajeTVaron.put(20,72);
+        puntajeTVaron.put(21,74);
+        puntajeTVaron.put(22,76);
+        puntajeTVaron.put(23,78);
+        puntajeTVaron.put(24,80);
+        puntajeTVaron.put(25,82);
+        puntajeTVaron.put(26,83);
+        puntajeTVaron.put(27,85);
+        puntajeTVaron.put(28,87);
+        puntajeTVaron.put(29,89);
+        puntajeTVaron.put(30,91);
+        puntajeTVaron.put(31,93);
+        puntajeTVaron.put(32,95);
+        puntajeTVaron.put(33,97);
+
+
+        puntajeTMujer.put(0,32);
+        puntajeTMujer.put(1,34);
+        puntajeTMujer.put(2,36);
+        puntajeTMujer.put(3,38);
+        puntajeTMujer.put(4,40);
+        puntajeTMujer.put(5,41);
+        puntajeTMujer.put(6,43);
+        puntajeTMujer.put(7,45);
+        puntajeTMujer.put(8,47);
+        puntajeTMujer.put(9,49);
+        puntajeTMujer.put(10,50);
+        puntajeTMujer.put(11,52);
+        puntajeTMujer.put(12,54);
+        puntajeTMujer.put(13,56);
+        puntajeTMujer.put(14,58);
+        puntajeTMujer.put(15,59);
+        puntajeTMujer.put(16,61);
+        puntajeTMujer.put(17,63);
+        puntajeTMujer.put(18,65);
+        puntajeTMujer.put(19,67);
+        puntajeTMujer.put(20,68);
+        puntajeTMujer.put(21,70);
+        puntajeTMujer.put(22,72);
+        puntajeTMujer.put(23,74);
+        puntajeTMujer.put(24,76);
+        puntajeTMujer.put(25,77);
+        puntajeTMujer.put(26,79);
+        puntajeTMujer.put(27,81);
+        puntajeTMujer.put(28,83);
+        puntajeTMujer.put(29,85);
+        puntajeTMujer.put(30,86);
+        puntajeTMujer.put(31,88);
+        puntajeTMujer.put(32,90);
+        puntajeTMujer.put(33,92);
 
 
     }
