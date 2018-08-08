c9eff0247ca6a44f8cf91854dd7acfaa9fe0d903
GonzaV <vieygonzalo@gmail.com>
2018-08-08 15:16:56 -0300

    Termino escala de cinismo, escalas de contenido

--- src/main/java/Model/Escalas/Contenido/Cinismo.java
+++ src/main/java/Model/Escalas/Contenido/Cinismo.java
@@ -25,6 +25,41 @@ public class Cinismo extends Escala {
         puntajeTVaron.put(12,52);
         puntajeTVaron.put(13,54);
         puntajeTVaron.put(14,56);
+        puntajeTVaron.put(15,58);
+        puntajeTVaron.put(16,60);
+        puntajeTVaron.put(17,62);
+        puntajeTVaron.put(18,64);
+        puntajeTVaron.put(19,66);
+        puntajeTVaron.put(20,67);
+        puntajeTVaron.put(21,69);
+        puntajeTVaron.put(22,71);
+        puntajeTVaron.put(23,73);
+
+
+        puntajeTMujer.put(0,30);
+        puntajeTMujer.put(1,32);
+        puntajeTMujer.put(2,34);
+        puntajeTMujer.put(3,36);
+        puntajeTMujer.put(4,38);
+        puntajeTMujer.put(5,40);
+        puntajeTMujer.put(6,42);
+        puntajeTMujer.put(7,44);
+        puntajeTMujer.put(8,46);
+        puntajeTMujer.put(9,48);
+        puntajeTMujer.put(10,50);
+        puntajeTMujer.put(11,52);
+        puntajeTMujer.put(12,54);
+        puntajeTMujer.put(13,56);
+        puntajeTMujer.put(14,58);
+        puntajeTMujer.put(15,60);
+        puntajeTMujer.put(16,62);
+        puntajeTMujer.put(17,64);
+        puntajeTMujer.put(18,66);
+        puntajeTMujer.put(19,68);
+        puntajeTMujer.put(20,70);
+        puntajeTMujer.put(21,72);
+        puntajeTMujer.put(22,74);
+        puntajeTMujer.put(23,76);
 
 
     }
