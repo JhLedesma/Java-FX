5845e1e8f3c9f675476daeb83cce923ad6b2a63a
GonzaV <vieygonzalo@gmail.com>
2018-08-08 15:57:14 -0300

    Agrego escala Baja autoestima, escalas de contenido

---
+++ src/main/java/Model/Escalas/Contenido/Baja_Autoestima.java
@@ -0,0 +1,70 @@
+package Model.Escalas.Contenido;
+
+import Model.Escalas.Escala;
+
+public class Baja_Autoestima extends Escala {
+
+
+    @Override
+    protected void iniciarItemsEscala() {
+
+    }
+
+    @Override
+    protected void inicarPT() {
+
+        puntajeTMujer.put(0,32);
+        puntajeTMujer.put(1,34);
+        puntajeTMujer.put(2,36);
+        puntajeTMujer.put(3,39);
+        puntajeTMujer.put(4,41);
+        puntajeTMujer.put(5,43);
+        puntajeTMujer.put(6,46);
+        puntajeTMujer.put(7,48);
+        puntajeTMujer.put(8,50);
+        puntajeTMujer.put(9,53);
+        puntajeTMujer.put(10,55);
+        puntajeTMujer.put(11,57);
+        puntajeTMujer.put(12,60);
+        puntajeTMujer.put(13,62);
+        puntajeTMujer.put(14,64);
+        puntajeTMujer.put(15,67);
+        puntajeTMujer.put(16,69);
+        puntajeTMujer.put(17,71);
+        puntajeTMujer.put(18,73);
+        puntajeTMujer.put(19,76);
+        puntajeTMujer.put(20,78);
+        puntajeTMujer.put(21,80);
+        puntajeTMujer.put(22,83);
+        puntajeTMujer.put(23,85);
+        puntajeTMujer.put(24,87);
+
+
+        puntajeTVaron.put(0,33);
+        puntajeTVaron.put(1,35);
+        puntajeTVaron.put(2,38);
+        puntajeTVaron.put(3,40);
+        puntajeTVaron.put(4,43);
+        puntajeTVaron.put(5,45);
+        puntajeTVaron.put(6,48);
+        puntajeTVaron.put(7,50);
+        puntajeTVaron.put(8,53);
+        puntajeTVaron.put(9,55);
+        puntajeTVaron.put(10,58);
+        puntajeTVaron.put(11,60);
+        puntajeTVaron.put(12,63);
+        puntajeTVaron.put(13,65);
+        puntajeTVaron.put(14,68);
+        puntajeTVaron.put(15,70);
+        puntajeTVaron.put(16,72);
+        puntajeTVaron.put(17,75);
+        puntajeTVaron.put(18,77);
+        puntajeTVaron.put(19,80);
+        puntajeTVaron.put(20,82);
+        puntajeTVaron.put(21,85);
+        puntajeTVaron.put(22,87);
+        puntajeTVaron.put(23,90);
+        puntajeTVaron.put(24,92);
+        
+    }
+}
