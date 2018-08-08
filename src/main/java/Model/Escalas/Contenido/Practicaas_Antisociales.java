dc41f0c77a0e57ec9a4794e85193c29cdcb7b6c5
GonzaV <vieygonzalo@gmail.com>
2018-08-08 15:29:54 -0300

    Agrego escala practicas antisociales, escalas de contenido

---
+++ src/main/java/Model/Escalas/Contenido/Practicaas_Antisociales.java
@@ -0,0 +1,63 @@
+package Model.Escalas.Contenido;
+
+import Model.Escalas.Escala;
+
+public class Practicaas_Antisociales extends Escala {
+
+
+    @Override
+    protected void iniciarItemsEscala() {
+
+    }
+
+    @Override
+    protected void inicarPT() {
+
+        puntajeTMujer.put(2,32);
+        puntajeTMujer.put(3,35);
+        puntajeTMujer.put(4,38);
+        puntajeTMujer.put(5,40);
+        puntajeTMujer.put(6,43);
+        puntajeTMujer.put(7,46);
+        puntajeTMujer.put(8,49);
+        puntajeTMujer.put(9,51);
+        puntajeTMujer.put(10,54);
+        puntajeTMujer.put(11,57);
+        puntajeTMujer.put(12,60);
+        puntajeTMujer.put(13,62);
+        puntajeTMujer.put(14,65);
+        puntajeTMujer.put(15,68);
+        puntajeTMujer.put(16,71);
+        puntajeTMujer.put(17,73);
+        puntajeTMujer.put(18,76);
+        puntajeTMujer.put(19,79);
+        puntajeTMujer.put(20,82);
+        puntajeTMujer.put(21,84);
+        puntajeTMujer.put(22,87);
+
+
+        puntajeTVaron.put(2,31);
+        puntajeTVaron.put(3,33);
+        puntajeTVaron.put(4,36);
+        puntajeTVaron.put(5,38);
+        puntajeTVaron.put(6,41);
+        puntajeTVaron.put(7,43);
+        puntajeTVaron.put(8,46);
+        puntajeTVaron.put(9,48);
+        puntajeTVaron.put(10,50);
+        puntajeTVaron.put(11,53);
+        puntajeTVaron.put(12,55);
+        puntajeTVaron.put(13,58);
+        puntajeTVaron.put(14,60);
+        puntajeTVaron.put(15,63);
+        puntajeTVaron.put(16,65);
+        puntajeTVaron.put(17,68);
+        puntajeTVaron.put(18,70);
+        puntajeTVaron.put(19,72);
+        puntajeTVaron.put(20,75);
+        puntajeTVaron.put(21,77);
+        puntajeTVaron.put(22,80);
+
+
+    }
+}
