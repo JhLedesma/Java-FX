0dae8f0aa2067d16e6aab5eca1ee60141a0b67f7
GonzaV <vieygonzalo@gmail.com>
2018-08-09 13:35:36 -0300

    Agrego escala Indicadores negativos de tratamiento, escalas de contenido. Fin de escalas de contenido

---
+++ src/main/java/Model/Escalas/Contenido/Indicadores_Negativos_De_Tratamiento.java
@@ -0,0 +1,74 @@
+package Model.Escalas.Contenido;
+
+import Model.Escalas.Escala;
+
+public class Indicadores_Negativos_De_Tratamiento extends Escala {
+
+
+    @Override
+    protected void iniciarItemsEscala() {
+
+    }
+
+    @Override
+    protected void inicarPT() {
+
+        puntajeTMujer.put(0,33);
+        puntajeTMujer.put(1,35);
+        puntajeTMujer.put(2,37);
+        puntajeTMujer.put(3,39);
+        puntajeTMujer.put(4,41);
+        puntajeTMujer.put(5,43);
+        puntajeTMujer.put(6,45);
+        puntajeTMujer.put(7,48);
+        puntajeTMujer.put(8,50);
+        puntajeTMujer.put(9,52);
+        puntajeTMujer.put(10,54);
+        puntajeTMujer.put(11,56);
+        puntajeTMujer.put(12,58);
+        puntajeTMujer.put(13,60);
+        puntajeTMujer.put(14,63);
+        puntajeTMujer.put(15,65);
+        puntajeTMujer.put(16,67);
+        puntajeTMujer.put(17,69);
+        puntajeTMujer.put(18,71);
+        puntajeTMujer.put(19,73);
+        puntajeTMujer.put(20,75);
+        puntajeTMujer.put(21,78);
+        puntajeTMujer.put(22,80);
+        puntajeTMujer.put(23,82);
+        puntajeTMujer.put(24,84);
+        puntajeTMujer.put(25,86);
+        puntajeTMujer.put(26,88);
+
+
+        puntajeTVaron.put(0,34);
+        puntajeTMujer.put(1,36);
+        puntajeTMujer.put(2,39);
+        puntajeTMujer.put(3,41);
+        puntajeTMujer.put(4,43);
+        puntajeTMujer.put(5,45);
+        puntajeTMujer.put(6,47);
+        puntajeTMujer.put(7,49);
+        puntajeTMujer.put(8,51);
+        puntajeTMujer.put(9,53);
+        puntajeTMujer.put(10,55);
+        puntajeTMujer.put(11,57);
+        puntajeTMujer.put(12,59);
+        puntajeTMujer.put(13,61);
+        puntajeTMujer.put(14,63);
+        puntajeTMujer.put(15,66);
+        puntajeTMujer.put(16,68);
+        puntajeTMujer.put(17,70);
+        puntajeTMujer.put(18,72);
+        puntajeTMujer.put(19,74);
+        puntajeTMujer.put(20,76);
+        puntajeTMujer.put(21,78);
+        puntajeTMujer.put(22,80);
+        puntajeTMujer.put(23,82);
+        puntajeTMujer.put(24,84);
+        puntajeTMujer.put(25,86);
+        puntajeTMujer.put(26,88);
+
+    }
+}
