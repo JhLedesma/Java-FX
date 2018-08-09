28299b332abaf457e7981aac2ca3bfdac1738282
GonzaV <vieygonzalo@gmail.com>
2018-08-09 13:14:41 -0300

    Agrego escala Problemas Familiares, escalas de contenido

---
+++ src/main/java/Model/Escalas/Contenido/Problemas_Familiares.java
@@ -0,0 +1,72 @@
+package Model.Escalas.Contenido;
+
+import Model.Escalas.Escala;
+
+public class Problemas_Familiares extends Escala {
+
+
+    @Override
+    protected void iniciarItemsEscala() {
+
+    }
+
+    @Override
+    protected void inicarPT() {
+
+        puntajeTMujer.put(0,34);
+        puntajeTMujer.put(1,36);
+        puntajeTMujer.put(2,38);
+        puntajeTMujer.put(3,41);
+        puntajeTMujer.put(4,43);
+        puntajeTMujer.put(5,46);
+        puntajeTMujer.put(6,48);
+        puntajeTMujer.put(7,50);
+        puntajeTMujer.put(8,53);
+        puntajeTMujer.put(9,55);
+        puntajeTMujer.put(10,57);
+        puntajeTMujer.put(11,60);
+        puntajeTMujer.put(12,62);
+        puntajeTMujer.put(13,65);
+        puntajeTMujer.put(14,67);
+        puntajeTMujer.put(15,69);
+        puntajeTMujer.put(16,72);
+        puntajeTMujer.put(17,74);
+        puntajeTMujer.put(18,76);
+        puntajeTMujer.put(19,79);
+        puntajeTMujer.put(20,81);
+        puntajeTMujer.put(21,84);
+        puntajeTMujer.put(22,86);
+        puntajeTMujer.put(23,88);
+        puntajeTMujer.put(24,91);
+        puntajeTMujer.put(25,93);
+
+
+        puntajeTVaron.put(0,35);
+        puntajeTVaron.put(1,38);
+        puntajeTVaron.put(2,40);
+        puntajeTVaron.put(3,43);
+        puntajeTVaron.put(4,45);
+        puntajeTVaron.put(5,47);
+        puntajeTVaron.put(6,50);
+        puntajeTVaron.put(7,52);
+        puntajeTVaron.put(8,55);
+        puntajeTVaron.put(9,57);
+        puntajeTVaron.put(10,59);
+        puntajeTVaron.put(11,62);
+        puntajeTVaron.put(12,64);
+        puntajeTVaron.put(13,67);
+        puntajeTVaron.put(14,69);
+        puntajeTVaron.put(15,71);
+        puntajeTVaron.put(16,74);
+        puntajeTVaron.put(17,76);
+        puntajeTVaron.put(18,79);
+        puntajeTVaron.put(19,81);
+        puntajeTVaron.put(20,83);
+        puntajeTVaron.put(21,86);
+        puntajeTVaron.put(22,88);
+        puntajeTVaron.put(23,91);
+        puntajeTVaron.put(24,93);
+        puntajeTVaron.put(25,95);
+
+    }
+}
