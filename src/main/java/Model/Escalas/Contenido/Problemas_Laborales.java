a2cfec9b47f49fa479f1766d01e36031b93ab1e2
GonzaV <vieygonzalo@gmail.com>
2018-08-09 13:27:46 -0300

    Agrego escala Problemas Laborales, escalas de contenido

---
+++ src/main/java/Model/Escalas/Contenido/Problemas_Laborales.java
@@ -0,0 +1,88 @@
+package Model.Escalas.Contenido;
+
+import Model.Escalas.Escala;
+
+public class Problemas_Laborales extends Escala {
+
+
+    @Override
+    protected void iniciarItemsEscala() {
+
+    }
+
+    @Override
+    protected void inicarPT() {
+
+        puntajeTVaron.put(0,34);
+        puntajeTVaron.put(1,36);
+        puntajeTVaron.put(2,37);
+        puntajeTVaron.put(3,39);
+        puntajeTVaron.put(4,41);
+        puntajeTVaron.put(5,42);
+        puntajeTVaron.put(6,44);
+        puntajeTVaron.put(7,46);
+        puntajeTVaron.put(8,47);
+        puntajeTVaron.put(9,49);
+        puntajeTVaron.put(10,51);
+        puntajeTVaron.put(11,52);
+        puntajeTVaron.put(12,54);
+        puntajeTVaron.put(13,56);
+        puntajeTVaron.put(14,57);
+        puntajeTVaron.put(15,59);
+        puntajeTVaron.put(16,61);
+        puntajeTVaron.put(17,62);
+        puntajeTVaron.put(18,64);
+        puntajeTVaron.put(19,66);
+        puntajeTVaron.put(20,67);
+        puntajeTVaron.put(21,69);
+        puntajeTVaron.put(22,71);
+        puntajeTVaron.put(23,72);
+        puntajeTVaron.put(24,74);
+        puntajeTVaron.put(25,76);
+        puntajeTVaron.put(26,77);
+        puntajeTVaron.put(27,79);
+        puntajeTVaron.put(28,81);
+        puntajeTVaron.put(29,82);
+        puntajeTVaron.put(30,84);
+        puntajeTVaron.put(31,86);
+        puntajeTVaron.put(32,87);
+        puntajeTVaron.put(33,89);
+
+
+        puntajeTMujer.put(0,32);
+        puntajeTMujer.put(1,34);
+        puntajeTMujer.put(2,35);
+        puntajeTMujer.put(3,37);
+        puntajeTMujer.put(4,39);
+        puntajeTMujer.put(5,40);
+        puntajeTMujer.put(6,42);
+        puntajeTMujer.put(7,44);
+        puntajeTMujer.put(8,45);
+        puntajeTMujer.put(9,47);
+        puntajeTMujer.put(10,49);
+        puntajeTMujer.put(11,50);
+        puntajeTMujer.put(12,52);
+        puntajeTMujer.put(13,54);
+        puntajeTMujer.put(14,55);
+        puntajeTMujer.put(15,57);
+        puntajeTMujer.put(16,59);
+        puntajeTMujer.put(17,60);
+        puntajeTMujer.put(18,62);
+        puntajeTMujer.put(19,64);
+        puntajeTMujer.put(20,65);
+        puntajeTMujer.put(21,67);
+        puntajeTMujer.put(22,69);
+        puntajeTMujer.put(23,70);
+        puntajeTMujer.put(24,72);
+        puntajeTMujer.put(25,74);
+        puntajeTMujer.put(26,75);
+        puntajeTMujer.put(27,77);
+        puntajeTMujer.put(28,79);
+        puntajeTMujer.put(29,80);
+        puntajeTMujer.put(30,82);
+        puntajeTMujer.put(31,84);
+        puntajeTMujer.put(32,85);
+        puntajeTMujer.put(33,87);
+
+    }
+}
